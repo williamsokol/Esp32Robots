7a71eb71ab7e7315ef9fa6f8a2a2ce01430477d9
Neur0plasticity <41029495+Neur0plasticity@users.noreply.github.com>
2022-04-23 13:16:51 -0700

    Add files via upload

--- NodeJS-Registry-Server/app.js
+++ NodeJS-Registry-Server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const https = require("https");
+// const http = require('http');
+const axios = require('axios');
 
 const port = 8080;
 let connectionpairs = [
@@ -32,5 +33,57 @@ app.get('/unregister',function(req,res){
 });
 
 
-
 app.listen(port);
+
+
+
+
+function registerRobotToVideoStreamServer(req) {
+    // http.request(`${videoRegistryUrl}/${videoRegisterPath}`)
+    axios
+        .get(roboVideoRegistryUri)
+        .then(res => {
+            console.log(`statusCode: ${res.status}`)
+            console.log(res)
+        })
+        .catch(error => {
+            // console.error(error)
+            unregisterEverything(req);
+        })
+}
+function registerRobotToCommandStreamServer(req){
+    axios
+        .get(roboCommandRegistryUri)
+        .then(res => {
+            console.log(`statusCode: ${res.status}`)
+            console.log(res)
+        })
+        .catch(error => {
+            // console.error(error)
+            unregisterEverything(req);
+        })
+}
+function registerUItoVideoStreamServer(req){
+    axios
+        .get(uiVideoRegistryUri)
+        .then(res => {
+            console.log(`statusCode: ${res.status}`)
+            console.log(res)
+        })
+        .catch(error => {
+            // console.error(error)
+            unregisterEverything(req);
+        })
+}
+function registerUItoCommandStreamServer(req){
+    axios
+        .get(uiCommandRegistryUri)
+        .then(res => {
+            console.log(`statusCode: ${res.status}`)
+            console.log(res)
+        })
+        .catch(error => {
+            // console.error(error)
+            unregisterEverything(req);
+        })
+}
