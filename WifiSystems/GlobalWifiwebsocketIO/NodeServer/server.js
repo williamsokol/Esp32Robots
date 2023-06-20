8924bd61619f9164cb8dd46070a506b5b040d7ba
unknown <williamsokol0@gmail.com>
2023-06-19 17:44:23 -0700

    tons of server stuff like the lobby and making ws timeout

--- WifiSystems/GlobalWifiwebsocketIO/NodeServer/server.js
+++ WifiSystems/GlobalWifiwebsocketIO/NodeServer/server.js
@@ -1,6 +1,8 @@
+const crud = require('./CRUD.js');
 const path = require('path');
 const express = require('express');
 const WebSocket = require('ws');
+const url = require('url');
 const app = express();
 
 const WS_PORT  = 8888;
@@ -9,42 +11,94 @@ const HTTP_PORT = 8000;
 const wsServer = new WebSocket.Server({port: WS_PORT}, ()=> console.log(`WS Server is listening at ${WS_PORT}`));
 
 let connectedClients = [];
-let robot = null
-wsServer.on('connection', (ws, req)=>{
-    console.log('Connected');
+const robots = []
+async function start() {
     
+    // console.log(await crud.readlist());
+}start()
+
+wsServer.on('connection', async (ws, req)=>{
+    
+    console.log('Connected to: ' + req.headers['type']);
+    //setUpConnection(ws,req);
+    //handleRobotConnection(ws)
+    // Object.keys(req.headers).forEach((key) => {
+    //     const value = req.headers[key];
+    //     console.log(`${key}: ${value}`);
+    // }); 
+   
+        
     if(req.headers['type'] == "robot")
     {
-        robot = ws;   
+        //handle robot dataIn
+        handleRobotConnection(ws,req);
     }else{
-        connectedClients.push(ws);
+        // handle client dataIn
+        handleWebConnection(ws,req);
     }
+  
 
-    ws.on('message', data => {
-        
-        if(req.headers['type'] == "robot")
-        {
-            connectedClients.forEach((ws,i)=>{
-                if(ws.readyState === ws.OPEN){
-                    ws.send(data);
-                }else{
-                    connectedClients.splice(i ,1);
-                }
-            })
-        }else{
-            
-            Object.keys(req.headers).forEach((key) => {
-                const value = req.headers[key];
-                console.log(`${key}: ${value}`);
-            }); 
-            if (robot != null){
-                console.log("sending to robot");
-                robot.send(data);
-            }
-          
-        }
-    });
+   
 });
 
 app.get('/client',(req,res)=>res.sendFile(path.resolve(__dirname, './client.html')));
+app.get('/LobbyList',(req,res)=>res.sendFile(path.resolve(__dirname, './LobbyList.html')));
 app.listen(HTTP_PORT, ()=> console.log(`HTTP server listening at ${HTTP_PORT}`));
+
+
+
+async function handleRobotConnection(ws,req)
+{
+    let lastActivityTime = Date.now();
+    const timeoutDuration = 60000;
+    
+    ws.id = await crud.smallestUnusedID()
+    console.log(ws.id);
+    robots.push(ws);   
+    crud.AddRobot("test","pass", ws.id);
+    console.log("adding robot to database")
+
+    ws.on('message', data => {
+        lastActivityTime = Date.now();
+
+        connectedClients.forEach((ws,i)=>{
+            if(ws.readyState === ws.OPEN){
+                ws.send(data);
+            }else{
+                connectedClients.splice(i ,1);
+            }
+        })
+    })
+
+    ws.on('close', data => {
+        console.log("robot websocket closed");
+        if(req.headers['type'] == "robot")
+            crud.deleteRow(req.headers['id']);
+    })
+
+    let timeout = setInterval(() => {
+        const elapsedTime = Date.now() - lastActivityTime;
+        console.log("test connections ");
+        if (elapsedTime >= timeoutDuration) {
+          ws.terminate(); // Close the WebSocket connection
+          clearInterval(timeout);
+        }
+    }, 10000);
+}
+async function handleWebConnection(ws,req)
+{
+    connectedClients.push(ws);
+
+    let queryParams = url.parse(req.url, true).query;
+    console.log('Query parameters:', queryParams);
+
+    let robot = robots.find(r => r.id == queryParams['id']);
+    //console.log(robots[0].id)
+    ws.on('message', data => {
+        if (robot != null){
+            console.log("sending to robot");
+            robot.send(data);
+        }
+    })
+}
+// console.log(Object.keys(base[i]));
